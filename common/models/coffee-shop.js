12b50c7995d252991a1d02f4e95727ccc9e0d14e
amit pareek <ampareek@in.ibm.com>
2019-01-06 11:20:03 +0530

    first checkin

---
+++ common/models/coffee-shop.js
@@ -0,0 +1,68 @@
+'use strict';
+
+module.exports = function (Coffeeshop) {
+
+    Coffeeshop.status = function (cb) {
+        var currentDate = new Date();
+        var currentHour = currentDate.getHours();
+        var OPEN_HOUR = 6;
+        var CLOSE_HOUR = 20;
+        console.log('Current hour is %d', currentHour);
+        var response;
+        if (currentHour >= OPEN_HOUR && currentHour < CLOSE_HOUR) {
+            response = 'We are open for business.';
+        } else {
+            response = 'Sorry, we are closed. Open daily from 6am to 8pm.';
+        }
+        cb(null, response);
+    };
+    Coffeeshop.getName = function(shopId, cb) {
+        console.log("shopId ::"+shopId); 
+        Coffeeshop.findById( shopId, function (err, instance) {
+            console.log(instance);
+            console.log(err); 
+            if(!err){
+            var response = "Name of coffee shop is " + instance.name;
+            cb(null, response);
+            console.log(instance);
+            }
+            else{
+                cb(err, null);
+                console.log(err);  
+            }
+        });
+      }
+    Coffeeshop.remoteMethod(
+        'status', {
+            http: {
+                path: '/status',
+                verb: 'get'
+            },
+            returns: {
+                arg: 'status',
+                type: 'string'
+            }
+        }
+    );
+
+    Coffeeshop.remoteMethod(        
+        'getName',
+        {
+            http: { 
+                path: '/getname',
+                verb: 'get' 
+            },
+            accepts: { 
+                arg: 'id',
+                type: 'number', 
+                required: true, 
+                http: { 
+                    source: 'query' 
+                } 
+            },
+            returns: { arg: 'name', 
+                       type: 'string' 
+                }
+        }
+    );
+};
